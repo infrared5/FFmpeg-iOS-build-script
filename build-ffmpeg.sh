54962bbdcc0fd746fdc82220c0487c4d74b5ca7f
bustardcelly <bustardcelly@gmail.com>
2017-09-27 11:37:57 -0400

    Rename of `AVMediaType` to resolve conflicts
    The recent XCode/iOS updates have conflicting declaration types for `AVMediaType` when using ffmpeg and `AVFoundation`.
    
    As such, the declarations of `AVMediaType` has been changed in the ffmpeg source to `AVMediaTypeFFMPEG`.
    
    https://forums.developer.apple.com/thread/83067?q=AVMEdia

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -18,12 +18,12 @@ CONFIGURE_FLAGS="--enable-cross-compile --disable-debug --disable-programs \
 
 if [ "$X264" ]
 then
-	CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-gpl --enable-libx264"
+  CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-gpl --enable-libx264"
 fi
 
 if [ "$FDK_AAC" ]
 then
-	CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-libfdk-aac"
+  CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-libfdk-aac"
 fi
 
 # avresample
@@ -38,127 +38,130 @@ DEPLOYMENT_TARGET="6.0"
 
 if [ "$*" ]
 then
-	if [ "$*" = "lipo" ]
-	then
-		# skip compile
-		COMPILE=
-	else
-		ARCHS="$*"
-		if [ $# -eq 1 ]
-		then
-			# skip lipo
-			LIPO=
-		fi
-	fi
+  if [ "$*" = "lipo" ]
+  then
+    # skip compile
+    COMPILE=
+  else
+    ARCHS="$*"
+    if [ $# -eq 1 ]
+    then
+      # skip lipo
+      LIPO=
+    fi
+  fi
 fi
 
 if [ "$COMPILE" ]
 then
-	if [ ! `which yasm` ]
-	then
-		echo 'Yasm not found'
-		if [ ! `which brew` ]
-		then
-			echo 'Homebrew not found. Trying to install...'
+  if [ ! `which yasm` ]
+  then
+    echo 'Yasm not found'
+    if [ ! `which brew` ]
+    then
+      echo 'Homebrew not found. Trying to install...'
                         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
-				|| exit 1
-		fi
-		echo 'Trying to install Yasm...'
-		brew install yasm || exit 1
-	fi
-	if [ ! `which gas-preprocessor.pl` ]
-	then
-		echo 'gas-preprocessor.pl not found. Trying to install...'
-		(curl -L https://github.com/libav/gas-preprocessor/raw/master/gas-preprocessor.pl \
-			-o /usr/local/bin/gas-preprocessor.pl \
-			&& chmod +x /usr/local/bin/gas-preprocessor.pl) \
-			|| exit 1
-	fi
+        || exit 1
+    fi
+    echo 'Trying to install Yasm...'
+    brew install yasm || exit 1
+  fi
+  if [ ! `which gas-preprocessor.pl` ]
+  then
+    echo 'gas-preprocessor.pl not found. Trying to install...'
+    (curl -L https://github.com/libav/gas-preprocessor/raw/master/gas-preprocessor.pl \
+      -o /usr/local/bin/gas-preprocessor.pl \
+      && chmod +x /usr/local/bin/gas-preprocessor.pl) \
+      || exit 1
+  fi
 
-	if [ ! -r $SOURCE ]
-	then
-		echo 'FFmpeg source not found. Trying to download...'
-		curl http://www.ffmpeg.org/releases/$SOURCE.tar.bz2 | tar xj \
-			|| exit 1
-	fi
+  if [ ! -r $SOURCE ]
+  then
+    echo 'FFmpeg source not found. Trying to download...'
+    curl http://www.ffmpeg.org/releases/$SOURCE.tar.bz2 | tar xj \
+      || exit 1
+    # Traverse the ffmpeg source directory and replace: AVMediaType with AVMediaTypeFFMPEG
+    LC_ALL=C find ffmpeg-3.3 -type f -name '*.c' -exec sed -i '' s/AVMediaType/AVMediaTypeFFMPEG/g {} +
+    LC_ALL=C find ffmpeg-3.3 -type f -name '*.h' -exec sed -i '' s/AVMediaType/AVMediaTypeFFMPEG/g {} +
+  fi
 
-	CWD=`pwd`
-	for ARCH in $ARCHS
-	do
-		echo "building $ARCH..."
-		mkdir -p "$SCRATCH/$ARCH"
-		cd "$SCRATCH/$ARCH"
+  CWD=`pwd`
+  for ARCH in $ARCHS
+  do
+    echo "building $ARCH..."
+    mkdir -p "$SCRATCH/$ARCH"
+    cd "$SCRATCH/$ARCH"
 
-		CFLAGS="-arch $ARCH"
-		if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
-		then
-		    PLATFORM="iPhoneSimulator"
-		    CFLAGS="$CFLAGS -mios-simulator-version-min=$DEPLOYMENT_TARGET"
-		else
-		    PLATFORM="iPhoneOS"
-		    CFLAGS="$CFLAGS -mios-version-min=$DEPLOYMENT_TARGET -fembed-bitcode"
-		    if [ "$ARCH" = "arm64" ]
-		    then
-		        EXPORT="GASPP_FIX_XCODE5=1"
-		    fi
-		fi
+    CFLAGS="-arch $ARCH"
+    if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
+    then
+        PLATFORM="iPhoneSimulator"
+        CFLAGS="$CFLAGS -mios-simulator-version-min=$DEPLOYMENT_TARGET"
+    else
+        PLATFORM="iPhoneOS"
+        CFLAGS="$CFLAGS -mios-version-min=$DEPLOYMENT_TARGET -fembed-bitcode"
+        if [ "$ARCH" = "arm64" ]
+        then
+            EXPORT="GASPP_FIX_XCODE5=1"
+        fi
+    fi
 
-		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
-		CC="xcrun -sdk $XCRUN_SDK clang"
+    XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+    CC="xcrun -sdk $XCRUN_SDK clang"
 
-		# force "configure" to use "gas-preprocessor.pl" (FFmpeg 3.3)
-		if [ "$ARCH" = "arm64" ]
-		then
-		    AS="gas-preprocessor.pl -arch aarch64 -- $CC"
-		else
-		    AS="$CC"
-		fi
+    # force "configure" to use "gas-preprocessor.pl" (FFmpeg 3.3)
+    if [ "$ARCH" = "arm64" ]
+    then
+        AS="gas-preprocessor.pl -arch aarch64 -- $CC"
+    else
+        AS="$CC"
+    fi
 
-		CXXFLAGS="$CFLAGS"
-		LDFLAGS="$CFLAGS"
-		if [ "$X264" ]
-		then
-			CFLAGS="$CFLAGS -I$X264/include"
-			LDFLAGS="$LDFLAGS -L$X264/lib"
-		fi
-		if [ "$FDK_AAC" ]
-		then
-			CFLAGS="$CFLAGS -I$FDK_AAC/include"
-			LDFLAGS="$LDFLAGS -L$FDK_AAC/lib"
-		fi
+    CXXFLAGS="$CFLAGS"
+    LDFLAGS="$CFLAGS"
+    if [ "$X264" ]
+    then
+      CFLAGS="$CFLAGS -I$X264/include"
+      LDFLAGS="$LDFLAGS -L$X264/lib"
+    fi
+    if [ "$FDK_AAC" ]
+    then
+      CFLAGS="$CFLAGS -I$FDK_AAC/include"
+      LDFLAGS="$LDFLAGS -L$FDK_AAC/lib"
+    fi
 
-		TMPDIR=${TMPDIR/%\/} $CWD/$SOURCE/configure \
-		    --target-os=darwin \
-		    --arch=$ARCH \
-		    --cc="$CC" \
-		    --as="$AS" \
-		    $CONFIGURE_FLAGS \
-		    --extra-cflags="$CFLAGS" \
-		    --extra-ldflags="$LDFLAGS" \
-		    --prefix="$THIN/$ARCH" \
-		|| exit 1
+    TMPDIR=${TMPDIR/%\/} $CWD/$SOURCE/configure \
+        --target-os=darwin \
+        --arch=$ARCH \
+        --cc="$CC" \
+        --as="$AS" \
+        $CONFIGURE_FLAGS \
+        --extra-cflags="$CFLAGS" \
+        --extra-ldflags="$LDFLAGS" \
+        --prefix="$THIN/$ARCH" \
+    || exit 1
 
-		make -j3 install $EXPORT || exit 1
-		cd $CWD
-	done
+    make -j3 install $EXPORT || exit 1
+    cd $CWD
+  done
 fi
 
 if [ "$LIPO" ]
 then
-	echo "building fat binaries..."
-	mkdir -p $FAT/lib
-	set - $ARCHS
-	CWD=`pwd`
-	cd $THIN/$1/lib
-	for LIB in *.a
-	do
-		cd $CWD
-		echo lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB 1>&2
-		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB || exit 1
-	done
+  echo "building fat binaries..."
+  mkdir -p $FAT/lib
+  set - $ARCHS
+  CWD=`pwd`
+  cd $THIN/$1/lib
+  for LIB in *.a
+  do
+    cd $CWD
+    echo lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB 1>&2
+    lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB || exit 1
+  done
 
-	cd $CWD
-	cp -rf $THIN/$1/include $FAT
+  cd $CWD
+  cp -rf $THIN/$1/include $FAT
 fi
 
 echo Done
